9badb0a5eaa3499adecd142d9c2796cc4ad63f2f
Sam Doctolero <scdoctol@ucalgary.ca>
2015-03-11 18:52:37 -0600

    no message

--- Code/SAM/SAM/Servo.cpp
+++ Code/SAM/SAM/Servo.cpp
@@ -8,9 +8,26 @@ Uses softPwm.c functions from WiringPi (Gordon Projects)
 #include "Servo.h"
 #include "softPwm.h"
 
+#define WHEEL_RADIUS 26; //in CM (measure wheel radius)
+
+//Define class global variables
+struct Wheel
+{
+	int Pin;
+};
+
+
+
+int Travel(int distance) 
+{
+	//Figure out distance and duration of loop relationship 
+	//softPwmWrite()
+}
+
 
 Servo::Servo()
 {
+
 }
 
 
