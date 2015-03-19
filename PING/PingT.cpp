58c14156e2bb0d080a59b7ce6b6f3f1deff86cf2
Janelle <janjan@janjans-macbook-pro.local>
2015-03-18 19:33:45 -0600

    Hello! modified PingT.cpp

--- PING/PingT.cpp
+++ PING/PingT.cpp
@@ -12,7 +12,7 @@ int main()
 {
 	if (wiringPiSetup() == -1)
 	{
-		cout << "WiringPi setup faied..." << endl;
+		cout << "WiringPi setup failed..." << endl;
 		exit(1); //If setup fails then exit the main program
 	}
 
@@ -20,7 +20,7 @@ int main()
 	{
 		pinMode(PING, OUTPUT);
 		digitalWrite(PING, 0); //Reset to zero
-		delay(1);
+		delay(2);
 		//Start of transmission
 		digitalWrite(PING, 1);
 		usleep(5);
@@ -28,22 +28,24 @@ int main()
 		//Activated PING to scan
 		//Change pin mode to accept signals
 		pinMode(PING, INPUT);
-		while (digitalRead(PING) == 0){ usleep(1); } //wait for the signal to turn high
+		//while (digitalRead(PING) == 0){ usleep(1); } //wait for the signal to turn high
+		usleep(750);
+		//usleep = pulseIn(PING,1);
 		//clock_t initTime = clock();
 		//once the signal is high then measure how long it stays high;
 		double utime = 0;
 		while (digitalRead(PING) == 1)
 		{
-			usleep(0.5);
-			utime += 0.5;
+			usleep(1);
+			utime += 1;
 		}
 		//float timeDiff = (clock() - initTime) / (float)CLOCKS_PER_SEC; //seconds
 		double dist = (utime / 2000) * SOUND_SPEED; // (t/2)*(344m/s)*(100cm/1m)*(1s/1000us)
-
+		//double dist = utime/29/2;
 		cout << "Duration(us): " << utime << endl;
 		cout << "Distance(cm): " << dist << endl;
 		delay(1000);
-	}
+	}1
 	return 0;
 
 }
