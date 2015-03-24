058fbd4d312132d9639d8be1f38f011016bc4074
whosnext <jenny1208xing@hotmail.com>
2015-03-24 17:03:50 -0600

    no message

--- Code/SAM/Sensors.cpp
+++ Code/SAM/Sensors.cpp
@@ -17,7 +17,8 @@ Sensors::Sensors()
 	PhotoR = 6;
 	toggleRx = 7;
 	toggleTx = 0;
-
+    
+    topDist = getDistance(pingTop);
 	pinMode(toggleRx, INPUT);
 	pinMode(toggleTx, OUTPUT);
 	digitalWrite(toggleTx, 1);
@@ -25,25 +26,25 @@ Sensors::Sensors()
 
 Sensors::~Sensors(){}
 
-double getDistance(int ping)
+double Sensors::getDistance(int ping)
 {
 	pinMode(ping, OUTPUT);
-	digitalWrite(PING, 0); //Reset to zero
+	digitalWrite(ping, 0); //Reset to zero
 	delay(2);
 	//Start of transmission
-	digitalWrite(PING, 1);
+	digitalWrite(ping, 1);
 	delayMicroseconds(5);
-	digitalWrite(PING, 0);
+	digitalWrite(ping, 0);
 	//Activated PING to scan
 	//Change pin mode to accept signals
-	pinMode(PING, INPUT);
+	pinMode(ping, INPUT);
 	//while (digitalRead(PING) == 0){ usleep(1); } //wait for the signal to turn high
 	delayMicroseconds(750);
 	//usleep = pulseIn(PING,1);
 	//clock_t initTime = clock();
 	//once the signal is high then measure how long it stays high;
 	double utime = 0;
-	while (digitalRead(PING) == 1)
+	while (digitalRead(ping) == 1)
 	{
 		delayMicroseconds(8);
 		utime += 8;
@@ -54,17 +55,17 @@ double getDistance(int ping)
 }
 void Sensors::WaitForLight()
 {
-	pinMode(PHREST, INPUT);
+	pinMode(PhotoR, INPUT);
 	int read = 0;
 	while (read == 0)
 	{
 		//if key pressed then exit
-		read = digitalRead(PHREST);
+		read = digitalRead(PhotoR);
 		//delay(1);
 	}
 }
 
-int Sensors::BackDistance()
+double Sensors::BackDistance()
 {
 	return getDistance(pingBack);
 }
@@ -74,12 +75,15 @@ int Sensors::getToggle()
 	return toggleRx;
 }
 
-int Sensors::DetectEdge()
+int Sensors::DetectEdge(double dist_base)
 {
-	double dist = getDistance(pingTop);
-	//JENNY's CODE!!!!!!!!
 
-
+    if(getDistance(pingTop) <= dist_base)
+    //if ((abs(getDistance(pingTop)) - dist_base)<2)
+    { return 0;
+    }
+    else
+	return 1;
 
 
 
--- Code/SAM/Sensors.h
+++ Code/SAM/Sensors.h
@@ -2,7 +2,7 @@
 #include <iostream>
 
 using namespace std;
-using namespace LibSerial;
+
 
 //#define WHEEL_RADIUS 3.3
 //#define BOT_RADIUS 5.8
@@ -12,13 +12,15 @@ class Sensors
 private:
 	int pingTop, pingBack, PhotoR, toggleRx, toggleTx;
 	int EdgeDistance();
-	double getDistance(int ping);
+    double topDist;
+    
+	
 public:
 	Sensors();
 	~Sensors();
-
+    double getDistance(int ping);
 	int getToggle();
-	int WaitForLight();
-	int DetectEdge();
-	int BackDistance();
-}
+	void WaitForLight();
+	int DetectEdge(double dist);
+	double BackDistance();
+};
